322d02b025450227c18c6c125d7137a6c62e93fc
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-07-05 21:25:00 +0800

    Fix null CustomModProperties from some containers

--- src/main/java/com/cleanroommc/catalogue/client/data/ForgeModData.java
+++ src/main/java/com/cleanroommc/catalogue/client/data/ForgeModData.java
@@ -80,19 +80,22 @@ public class ForgeModData implements IModData {
     @Nullable
     @Override
     public String getItemIcon() {
-        return this.info.getCustomModProperties().get("iconItem");
+        Map<String,String> props = this.info.getCustomModProperties();
+        return props != null ? props.get("iconItem") : null;
     }
 
     @Nullable
     @Override
     public String getImageIcon() {
-        return this.info.getCustomModProperties().get("iconFile");
+        Map<String,String> props = this.info.getCustomModProperties();
+        return props != null ? props.get("iconFile") : null;
     }
 
     @Nullable
     @Override
     public String getLicense() {
-        return this.info.getCustomModProperties().get("license");
+        Map<String,String> props = this.info.getCustomModProperties();
+        return props != null ? props.get("license") : null;
     }
 
     @Nullable
@@ -116,7 +119,8 @@ public class ForgeModData implements IModData {
     @Nullable
     @Override
     public String getIssueTracker() {
-        return this.info.getCustomModProperties().get("issueTrackerUrl");
+        Map<String,String> props = this.info.getCustomModProperties();
+        return props != null ? props.get("issueTrackerUrl") : null;
     }
 
     @Nullable
@@ -128,7 +132,8 @@ public class ForgeModData implements IModData {
     @Nullable
     @Override
     public String getBackground() {
-        return this.info.getCustomModProperties().get("backgroundFile");
+        Map<String,String> props = this.info.getCustomModProperties();
+        return props != null ? props.get("backgroundFile") : null;
     }
 
     @Nullable
